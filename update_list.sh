97b35b76eb52143f320df6f111ed99986ed087e2
TripleA <46062533+TripleA150@users.noreply.github.com>
2025-10-23 21:57:21 +1000

    Update update_list.sh

--- update_list.sh
+++ update_list.sh
@@ -4,12 +4,13 @@ OUTPUT="/opt/etc/nfqws/user.list"
 
 TMP_DIR="/tmp/blocklist"
 LIST_FILE="$TMP_DIR/domains_all.tmp"
+IPSET_FILE="/opt/etc/nfqws/ipset.list"
 EXCLUDE_FILE="$TMP_DIR/exclude.tmp"
 FINAL_FILE="$TMP_DIR/final_list.tmp"
 
 mkdir -p "$TMP_DIR"
 
-rm -f "$LIST_FILE" "$EXCLUDE_FILE" "$FINAL_FILE"
+rm -f "$LIST_FILE" "$IPSET_FILE" "$EXCLUDE_FILE" "$FINAL_FILE"
 
 DOMAIN_SOURCE_1="https://raw.githubusercontent.com/1andrevich/Re-filter-lists/main/domains_all.lst"
 DOMAIN_SOURCE_2="https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/download/include-hosts.txt"
@@ -17,6 +18,7 @@ DOMAIN_SOURCE_2="https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/mai
 curl -k -s "$DOMAIN_SOURCE_1" >> "$LIST_FILE"
 curl -k -s "$DOMAIN_SOURCE_2" >> "$LIST_FILE"
 
+curl -k -s "https://raw.githubusercontent.com/TripleA150/All-in/refs/heads/main/ipset.lst" > "$IPSET_FILE"
 curl -k -s "https://raw.githubusercontent.com/TripleA150/All-in/refs/heads/main/exclude.lst" > "$EXCLUDE_FILE"
 
 grep -v '^#' "$LIST_FILE" | grep -v '^$' | sed 's/[[:space:]]//g' | sort -u > "$FINAL_FILE"
